7ecdee8e9e246d086412b970e060da34c8fdb108
Adam <adam22daia11@gmail.com>
2024-04-04 10:49:03 +0200

    Ajout du front Angular

---
+++ Logic/Front-Angular/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { NavBar2Component } from './nav-bar2/nav-bar2.component';
+
+export const routes: Routes = [
+
+    {
+    path: '',
+    component:  NavBar2Component
+    }
+        
+];
