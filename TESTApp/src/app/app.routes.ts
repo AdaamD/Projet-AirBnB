e2398364684b06b13673771caadfb441e9f05e3c
Adam <adam22daia11@gmail.com>
2024-04-04 10:26:47 +0200

    test ajout

---
+++ TESTApp/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { Test2Component } from './test2/test2.component';
+
+
+
+export const routes: Routes = [
+    {path: '',
+component:  Test2Component}
+    
+  ];
